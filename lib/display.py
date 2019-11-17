554cdc771a35141172cafd9ea1498942c439957b
pxckets <49133592+pxckets@users.noreply.github.com>
2019-11-17 09:00:43 -0500

    Update display.py

--- lib/display.py
+++ lib/display.py
@@ -70,57 +70,47 @@ class Display(object):
             ))
 
             progress = str(round(complete))
-            blocks = 0
             display = ""
             spacing = ""
 
             if int(progress) <= 9:
-                blocks = 0
                 spacing = "                                        "
             elif int(progress) <= 15:
-                blocks = 1
                 display = "####"
                 spacing = "                                    "
             elif int(progress) <= 25:
-                blocks = 2
                 display = "########"
                 spacing = "                                "
             elif int(progress) <= 35:
-                blocks = 3
                 display = "############"
                 spacing = "                            "
             elif int(progress) <= 45:
-                blocks = 4
                 display = "################"
                 spacing = "                        "
             elif int(progress) <= 55:
-                blocks = 5
                 display = "####################"
                 spacing = "                    "
             elif int(progress) <= 65:
-                blocks = 6
                 display = "########################"
                 spacing = "                "
             elif int(progress) <= 75:
-                blocks = 7
                 display = "############################"
                 spacing = "      "
             elif int(progress) <= 85:
-                blocks = 8
                 display = "################################"
                 spacing = "        "
             elif int(progress) <= 95:
-                blocks = 9
                 display = "####################################"
                 spacing ="    "
             elif int(progress) == 100:
-                blocks = 10
                 display = "########################################"
                 spacing = ""
-
-            print("[" + display + " " + progress + "% " + spacing + "]" )
-
-
+            print("")
+            print("________________________________________")
+            print("|" + display + " " + progress + "% " + spacing + "|" )
+            print("________________________________________")
+            print("")
+            print("")
 
         else:
             printf(
